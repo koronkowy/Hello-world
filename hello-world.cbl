0f3a73a85ec427195ca581f0abb25e57729844f2
Lacey <lacey.kasten@gmail.com>
2024-09-10 11:47:28 -0700

    Initial commit

---
+++ hello-world.cbl
@@ -0,0 +1,11 @@
+IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       AUTHOR. KORONKOWY.
+       DATE-WRITTEN. 2024-09-10.
+       REMARKS. Trying out writing hello world in COBOL.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           MAIN-PROCEDURE.
+               DISPLAY "Hello, World!".
+               STOP RUN.
